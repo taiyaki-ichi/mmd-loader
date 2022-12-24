6a183b293f96fc64b0b86176bd5eaada34cd0310
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2022-12-24 15:01:08 +0900

    pmx_vertexの新規追加

--- src/pmx_data.hpp
+++ src/pmx_data.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include<utility>
+#include<array>
+#include<optional>
 
 namespace mmdl
 {
@@ -63,4 +65,45 @@ namespace mmdl
 
 	};
 
+
+
+	// 1頂点情報
+	// ボーンのインデックスの大きさについて、pmx_header.bone_index_sizeは1,2,4の値をとる
+	// その最大の値である4の大きさに対応したstd::uint32_tをデフォルトとして使用する
+	template<typename Vec2, typename Vec3, typename Vec4, template<typename> typename Container,
+		typename BoneIndex = std::uint32_t>
+		struct pmx_vertex
+	{
+		// 頂点座標
+		Vec3 position;
+
+		// 法線
+		Vec3 normal;
+
+		// uv
+		Vec2 uv;
+
+		// 追加のuv
+		// pmx_header.add_uv_numberの数だけ使用する
+		std::array<Vec4, 4> additional_uv{};
+
+		// 影響を受けるボーンの数とweightが対応している
+		// BDEF1ならweight[0]=1、そのほかは0
+		// BDEF2ならweight[0]!=0,weight[1]!=0、そのほかは0（ちなみにweight[0]+weight[1]=1）
+		// BDEF4ならweightは全て0でない
+		std::array<BoneIndex, 4> bone{ 0,0,0,0 };
+		std::array<float, 4> weight{ 0.f,0.f,0.f,0.f };
+
+		// SDEF用の補正行列
+		// SDEFはBDEF2と補正行列によって計算される
+		// sdef[0]：SDEF-C値
+		// sdef[1]：SDEF-R0値
+		// sdef[2]：SDEF-R1値
+		std::optional<std::array<Vec3, 3>> sdef = std::nullopt;
+
+		// エッジ倍率
+		// 材質のエッジサイズに対しての倍率値
+		float edge_magnification;
+
+	};
 }
