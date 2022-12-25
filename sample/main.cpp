15bd3a5608df689170fb9c2975659ad68fe4a38b
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2022-12-25 18:24:33 +0900

    load_vertexの呼び出しの引数の変更

--- sample/main.cpp
+++ sample/main.cpp
@@ -27,7 +27,7 @@ int main()
 	{
 		const wchar_t* file_path = L"../../3dmodel/ƒpƒCƒ‚ƒ“/”h–Ö.pmx";
 		std::ifstream file{ file_path ,std::ios::binary };
-		auto header = mmdl::load_header<std::uint16_t>(file);
+		auto header = mmdl::load_header<>(file);
 		auto info = mmdl::load_info<std::wstring>(file, header.encode);
 		auto vertex = mmdl::load_vertex<std::vector, vec2, vec3, vec4>(file, header.add_uv_number, header.bone_index_size);
 	}
@@ -35,7 +35,7 @@ int main()
 	{
 		const wchar_t* file_path = L"../../3dmodel/ƒpƒCƒ‚ƒ“/”h–Ö.pmx";
 		std::ifstream file{ file_path ,std::ios::binary };
-		auto header = mmdl::load_header<std::uint16_t>(file);
+		auto header = mmdl::load_header<>(file);
 		auto info = mmdl::load_info<std::string>(file, header.encode);
 		auto vertex = mmdl::load_vertex<std::vector, vec2, vec3, vec4>(file, header.add_uv_number, header.bone_index_size);
 	}
@@ -43,7 +43,7 @@ int main()
 	{
 		const wchar_t* file_path = L"../../3dmodel/‚Æ‚«‚Ì‚»‚çŒöŽ®mmd_ver2.1/‚Æ‚«‚Ì‚»‚ç.pmx";
 		std::ifstream file{ file_path ,std::ios::binary };
-		auto header = mmdl::load_header<std::uint16_t>(file);
+		auto header = mmdl::load_header<>(file);
 		auto info = mmdl::load_info<std::wstring>(file, header.encode);
 		auto vertex = mmdl::load_vertex<std::vector, vec2, vec3, vec4>(file, header.add_uv_number, header.bone_index_size);
 	}
@@ -51,7 +51,7 @@ int main()
 	{
 		const wchar_t* file_path = L"../../3dmodel/‚Æ‚«‚Ì‚»‚çŒöŽ®mmd_ver2.1/‚Æ‚«‚Ì‚»‚ç.pmx";
 		std::ifstream file{ file_path ,std::ios::binary };
-		auto header = mmdl::load_header<std::uint16_t>(file);
+		auto header = mmdl::load_header<>(file);
 		auto info = mmdl::load_info<std::string>(file, header.encode);
 		auto vertex = mmdl::load_vertex<std::vector, vec2, vec3, vec4>(file, header.add_uv_number, header.bone_index_size);
 	}
@@ -59,7 +59,7 @@ int main()
 	{
 		const wchar_t* file_path = L"../../3dmodel/kizunaai/kizunaai.pmx";
 		std::ifstream file{ file_path ,std::ios::binary };
-		auto header = mmdl::load_header<std::uint16_t>(file);
+		auto header = mmdl::load_header<>(file);
 		auto info = mmdl::load_info<std::wstring>(file, header.encode);
 		auto vertex = mmdl::load_vertex<std::vector, vec2, vec3, vec4>(file, header.add_uv_number, header.bone_index_size);
 	}
@@ -67,7 +67,7 @@ int main()
 	{
 		const wchar_t* file_path = L"../../3dmodel/kizunaai/kizunaai.pmx";
 		std::ifstream file{ file_path ,std::ios::binary };
-		auto header = mmdl::load_header<std::uint16_t>(file);
+		auto header = mmdl::load_header<>(file);
 		auto info = mmdl::load_info<std::string>(file, header.encode);
 		auto vertex = mmdl::load_vertex<std::vector, vec2, vec3, vec4>(file, header.add_uv_number, header.bone_index_size);
 	}
